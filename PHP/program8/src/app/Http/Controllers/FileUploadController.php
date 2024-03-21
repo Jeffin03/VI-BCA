cc131dec3ac653c9daaeac064700c6d29d2f260b
Jeffin03 <jeffyissac2203@gmail.com>
2024-03-21 23:43:43 +0530

    new laravel program

---
+++ PHP/program8/src/app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+	public function showUploadForm()
+	{
+		return view('upload');
+	}
+
+	public function upload(Request $request)
+	{
+		$request->validate([
+			'file'=>'required|mimes:jpg,png,jpeg|max:2048',
+		]);
+	}
+
+	if($request->file('file')->isValid()){
+		$file = $request->file('file');
+		$fileName = time().'_'.$file->getClientOriginalName();
+		$file->move(public_path('uploads'), $fileName);
+
+
+		return redirect('/upload')->with('success', 'File uploaded successfully.');
+	} else {
+
+		return redirect('/upload'->with('error','Invalid file.');
+	}
+
---
+++ PHP/program8/src/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileUploadController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/upload',[FileUploadController::class, 'showUploadForm']);
+Route::post('/upload',[FileUploadController::class, 'upload']);
+
