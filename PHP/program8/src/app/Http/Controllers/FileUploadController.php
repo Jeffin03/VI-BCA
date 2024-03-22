d4662de5500511849793e70dc183568f8cb50f24
Jeffin03 <jeffyissac2203@gmail.com>
2024-03-22 12:16:01 +0530

    update in config

--- PHP/program8/src/app/Http/Controllers/FileUploadController.php
+++ PHP/program8/src/app/Http/Controllers/FileUploadController.php
@@ -1,32 +1,36 @@
 <?php
 
+
 namespace App\Http\Controllers;
+
 
 use Illuminate\Http\Request;
 
+
 class FileUploadController extends Controller
 {
-	public function showUploadForm()
-	{
-		return view('upload');
-	}
-
-	public function upload(Request $request)
-	{
-		$request->validate([
-			'file'=>'required|mimes:jpg,png,jpeg|max:2048',
-		]);
-	}
-
-	if($request->file('file')->isValid()){
-		$file = $request->file('file');
-		$fileName = time().'_'.$file->getClientOriginalName();
-		$file->move(public_path('uploads'), $fileName);
+    public function showUploadForm()
+    {
+        return view('upload');
+    }
 
 
-		return redirect('/upload')->with('success', 'File uploaded successfully.');
-	} else {
+    public function upload(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:jpg,png,jpeg|max:2048',
+        ]);
 
-		return redirect('/upload'->with('error','Invalid file.');
-	}
 
+        if ($request->file('file')->isValid()) {
+            $file = $request->file('file');
+            $fileName = time() . '_' . $file->getClientOriginalName();
+            $file->move(public_path('uploads'), $fileName);
+
+
+            return redirect('/upload')->with('success', 'File uploaded successfully.');
+        } else {
+            return redirect('/upload')->with('error', 'Invalid file.');
+        }
+    }
+}
